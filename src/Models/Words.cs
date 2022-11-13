734b6fe4d8c783031d194d177eecadbf7865dbbb
Кирилл Соломатин <kir.solomatin@gmail.com>
2022-11-13 13:26:34 +0300

    Add INotifyPropertyChanged

---
+++ src/Models/Words.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictioanry_EngRus_WPF.src.Models
+{
+    internal class Words
+    {
+        #region DateWords
+        public int ID { get; private set; }
+        public string Name { get; private set; }
+        public char[] Word { get; private set; }
+
+        #endregion
+    }
+}
---
+++ src/ViewModels/ViewModelBase.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Dictioanry_EngRus_WPF.src.ViewModels
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void Set<T>(ref T field, T value, [CallerMemberName] string name = "")
+        {
+            if(!field.Equals(value))
+            {
+                field = value;
+                OnPropertyChanged(name);
+            }
+        }
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+        protected void OnPropertyChanged(params string[] names)
+        {
+            foreach (var name in names)
+            {
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+            }
+        }
+    }
+}
